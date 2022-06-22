0b7df4557ea0a5815292bcdad9d74a13a20ac505
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-06-22 22:55:51 +0300

    Add local storage feature

--- app.js
+++ app.js
@@ -6,32 +6,49 @@ const app = Vue.createApp({
             todoList: [],
             finishedList: [],
             newTodoId: 1,
-            datetime: "14-06-22 / 01:36"
+            todoIdList: []
+        }
+    },
+    mounted() {
+        let localTodoList = JSON.parse(localStorage.getItem('todoList'))
+        if (localTodoList == null) return localStorage.setItem('todoList', JSON.stringify([]))
+        if (localTodoList.length != 0) {
+            JSON.parse(localStorage.getItem('todoList')).forEach(element => {
+                this.todoList.push(element)    
+            });
         }
     },
     methods: {
-        addTodo() {
+        async addTodo() {
             if (this.todoText == null) {
                 alert("Bir görev eklemeyi unutmayın.")
             }
             else {
-                this.todoList.push({id:this.newTodoId, todo: this.todoText, time: this.currentTime()})
+                let todoData = { id: this.newTodoId, todo: this.todoText, time: this.currentTime() }
+                this.todoList.push(todoData)
+                let localTodoList = JSON.parse(localStorage.getItem('todoList'))
+                localTodoList.push(todoData)
+                localStorage.setItem('todoList', JSON.stringify(localTodoList))
                 this.newTodoId++
+                this.todoText = null
             }
         },
-        removeTodo(id){
-            this.todoList = this.todoList.filter(todo => todo.id !== id)
+        removeTodo(id) {
+            let localTodoList = JSON.parse(localStorage.getItem('todoList'))
+            localTodoList = localTodoList.filter(todo => todo.id !== id)
+            localStorage.setItem('todoList',JSON.stringify(localTodoList))
+            this.todoList = localTodoList
         },
-        markAsDone(id){
+        markAsDone(id) {
             for (let index = 0; index < this.todoList.length; index++) {
                 const element = this.todoList[index];
-                if(element.id == id){
+                if (element.id == id) {
                     this.finishedList.push(element)
                     this.removeTodo(id)
                 }
             }
         },
-        removeFinished(id){
+        removeFinished(id) {
             this.finishedList = this.finishedList.filter(finished => finished.id !== id)
         },
         currentTime() {
